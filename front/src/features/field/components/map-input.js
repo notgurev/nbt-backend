9a9896858fddd4cc9db60b749f76d927935f04cc
raineduc <hramil0120@gmail.com>
2021-05-08 00:43:40 +0300

    Implement creating polygon on map

--- front/src/features/field/components/map-input.js
+++ front/src/features/field/components/map-input.js
@@ -1,8 +1,46 @@
-import React from "react";
-import {Map, SearchControl} from "react-yandex-maps/dist/development/react-yandex-maps";
+import React, { useState, useCallback } from "react";
+import { Button, Map, SearchControl, Polygon, withYMaps } from "react-yandex-maps/dist/development/react-yandex-maps";
 
-export const MapInput = () => (
-  <Map defaultState={{ center: [55.751574, 37.573856], zoom: 9 }} width="100%" height="100vh">
-    <SearchControl options={{ float: 'right' }} />
-  </Map>
-);
+const MapInputTemplate = ({ymaps}) => {
+  const [polygonEmpty, isPolygonEmpty] = useState(true);
+  const [polygon, setPolygon] = useState(null);
+  const polygonRef = useCallback(polygon => {
+    if (!polygon) return;
+    setPolygon(polygon);
+    polygon.editor.startDrawing();
+
+    polygon.events.add('geometrychange', () => {
+      const coordinates = polygon.geometry.getCoordinates();
+      if (!coordinates.length || !coordinates[0].length) {
+        isPolygonEmpty(true);
+        return;
+      }
+      isPolygonEmpty(false);
+    });
+  }, []);
+
+  const buttonRef = useCallback(button => {
+    if (!button) return;
+    button.events.add('click', () => {
+      if (polygon) {
+        polygon.geometry.setCoordinates([]);
+        polygon.editor.startDrawing();
+      }
+    });
+  }, [polygon]);
+
+  return (
+    <Map
+      defaultState={{center: [55.751574, 37.573856], zoom: 9}}
+      width="100%"
+      height="100vh"
+      modules={["geoObject.addon.editor"]}
+    >
+      {!polygonEmpty && <Button data={{content: 'Удалить поле'}} options={{maxWidth: 128, float: 'left'}} instanceRef={buttonRef}/>}
+      <SearchControl options={{float: 'right'}}/>
+      <Polygon instanceRef={polygonRef} defaultGeometry={[]}/>
+    </Map>
+  );
+}
+
+export const MapInput = withYMaps(MapInputTemplate, true);

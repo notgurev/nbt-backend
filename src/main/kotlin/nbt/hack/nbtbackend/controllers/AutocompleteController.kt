bf311b684acf30ab1340e0f10469a13aa9a077f8
notgurev <notgurev@gmail.com>
2021-05-08 14:06:59 +0300

    AutocompleteController 404 fix

--- src/main/kotlin/nbt/hack/nbtbackend/controllers/AutocompleteController.kt
+++ src/main/kotlin/nbt/hack/nbtbackend/controllers/AutocompleteController.kt
@@ -3,22 +3,23 @@ package nbt.hack.nbtbackend.controllers
 import nbt.hack.nbtbackend.services.AutocompleteService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-// todo disable security
 // todo test with prefix == ""
 
-@RestController("/api/autocomplete")
+@RestController
+@RequestMapping("/api/autocomplete")
 class AutocompleteController @Autowired constructor(
     private val autocompleteService: AutocompleteService
 ) {
-    @GetMapping("cultures")
+    @GetMapping("/cultures")
     fun getCultureNames(@RequestParam prefix: String): List<String> {
         return autocompleteService.getCultures(prefix)
     }
 
-    @GetMapping("soils")
+    @GetMapping("/soils")
     fun getSoilNames(@RequestParam prefix: String): List<String> {
         return autocompleteService.getSoils(prefix)
     }

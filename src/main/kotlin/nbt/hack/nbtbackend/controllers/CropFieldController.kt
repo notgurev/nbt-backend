9d1dba62dd6484e63cfde3d81b006385abe9ed4d
Vsev0l0d <vsevolod628@gmail.com>
2021-05-08 14:52:45 +0300

    CropFieldController, User changes

--- src/main/kotlin/nbt/hack/nbtbackend/controllers/CropFieldController.kt
+++ src/main/kotlin/nbt/hack/nbtbackend/controllers/CropFieldController.kt
@@ -1,8 +1,37 @@
 package nbt.hack.nbtbackend.controllers
 
-import org.springframework.web.bind.annotation.RestController
+import nbt.hack.nbtbackend.model.CropField
+import nbt.hack.nbtbackend.services.CropFieldService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class CropFieldController {
+@RequestMapping("/api/cropFields")
+class CropFieldController @Autowired constructor(
+        val cropFieldService: CropFieldService,
+){
+    @GetMapping("/{id}")
+    fun getCropField(@PathVariable id: Long): CropField{
+        return cropFieldService.getCropField(id)
+    }
 
+    @GetMapping
+    fun getAllCropFields(@RequestParam ownerUsername: String): List<CropField>{
+        return cropFieldService.getAllCropFields(ownerUsername)
+    }
+
+    @PostMapping
+    fun addCropField(ownerUsername: String){
+        cropFieldService.addCropField(ownerUsername)
+    }
+
+    @PostMapping("/{id}")
+    fun updateCropField(@PathVariable id: Long){
+        cropFieldService.updateCropField(id)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteUsername(@PathVariable id: Long){
+        cropFieldService.deleteUsername(id)
+    }
 }
